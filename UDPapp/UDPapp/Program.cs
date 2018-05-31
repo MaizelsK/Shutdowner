d9fabc1210017e708e8ee62ae5ab463f2ba4c6a9
Maizels Kirill <domo.ddr@gmail.com>
2018-05-31 11:21:45 +0600

    Homework done!

---
+++ UDPapp/UDPapp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace UDPapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Нажмите любую клавишу и все компьютеры, получившие сообщение, выключаться!");
+            Console.ReadLine();
+
+            UdpClient client = new UdpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("224.1.8.5"), 3535));
+
+            byte[] data = Encoding.Default.GetBytes("shutdown /s /t 0");
+
+            try
+            {
+                client.Send(data, data.Length);
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            client.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UDPclientApp/UDPclientApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Diagnostics;
+
+namespace UDPclientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(3535);
+            udpClient.JoinMulticastGroup(IPAddress.Parse("224.1.8.5"), 50);
+
+            IPEndPoint ipEndPoint = null;
+
+            byte[] receiveData =  udpClient.Receive(ref ipEndPoint);
+            string command = Encoding.Default.GetString(receiveData);
+
+            Process.Start(command.Substring(0, 8), command.Substring(8));
+
+            udpClient.Close();
+        }
+    }
+}
